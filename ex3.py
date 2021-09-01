5f50c4a1671172b7f6854488c02ac3ad13f1e063
christopher warrilow <meth0d@comcast.net>
2021-08-31 21:39:53 -0700

    lpthw3 exercises
    Learning python 3 the hard way exercises

---
+++ ex3.py
@@ -0,0 +1,26 @@
+print("I will now count my chickens:")
+
+# adds and divides
+print("Hens", 25 + 30 / 6)
+# minus and multiply and modulus
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now i will count the eggs:")
+# multiply multiply subtract multiply modulus subtract divides multiply
+print(3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print("Is it true that 3 + 2 < 5 - 7?")
+# multiply lessthan subtract = True or False result
+print(3 + 2 < 5 - 7)
+# multiply
+print("What is 3 + 2?", 3 + 2)
+# subtract
+print("What is 5 - 7?", 5 - 7)
+
+print("Oh, that's why it's False.")
+
+print("How about some more.")
+# Prints out the text while doing True or False answers
+print("Is it greater?", 5 > -2)
+print("Is it greater or equal?", 5 >= -2)
+print("Is it less or equal?", 5 <= -2)
