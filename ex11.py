6452521c13d4d09233d416807be824fc26bee1cd
meth0d1244 <meth0d@comcast.net>
2021-09-01 14:57:06 -0700

    ex11.py done!

---
+++ ex11.py
@@ -0,0 +1,12 @@
+# Asking questions to allow user input.
+# Also saving answer in to a variable.
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("How much do you wiegh?", end=' ')
+weight = input()
+print("Whats your favorite color?", end=' ')
+color = input()
+
+print(f"So, you're {age} old, {height} tall and {weight} heavy, favorite color is {color}")
